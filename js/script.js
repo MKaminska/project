3ddf0d8ece0420a1ce65388a91ed696d8a3448a1
Marta <example@gmail.com>
2020-12-11 00:50:24 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// // alert("Hello");
+
+// // const result = confirm("Are you here?");
+// // console.log(result);
+
+// // const answer = +prompt('Are you 18', "18");
+// // console.log(answer +5);
+
+// const answers =[];
+
+// // answers[0] = prompt('What is your name?', "");
+// // answers[1] = prompt('What is your surname?', "");
+// // answers[2] = prompt('How old are you', "");
+
+// console.log(typeof(answers));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/$`);
+
